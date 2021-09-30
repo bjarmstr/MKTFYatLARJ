247e39622996808bcf8e55076e784232bb2eebb1
Jean Armstrong <bjarmstr@yahoo.com>
2021-09-30 07:33:27 -0600

    added ListingVM and category to Listing

--- MKTFY.Models/ViewModels/Listing/ListingVM.cs
+++ MKTFY.Models/ViewModels/Listing/ListingVM.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MKTFY.Models.ViewModels
+namespace MKTFY.Models.ViewModels.Listing
 {
-    class ListingVM
+    public class ListingVM
     {
-    }
+        public ListingVM() { }
+
+        // Constructor for populating a new ListingVM from a Listing entity
+        public ListingVM(Entities.Listing src)
+        {
+            Id = src.Id;
+            Product = src.Product;
+            Details = src.Details;
+            Price = src.Price;
+            Category = src.Category;
+
+        }
+
+         
+        public Guid Id { get; set; }
+
+        
+        public string Product { get; set; }
+
+        
+        public string Details { get; set; }
+
+        
+        public decimal Price { get; set; }
+
+        
+        public string Category { get; set; }
+
 }
