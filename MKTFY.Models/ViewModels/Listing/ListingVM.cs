5248e1db4d8a5a6ca77ad7c590eb347635cdda39
Jean Armstrong <bjarmstr@yahoo.com>
2021-09-30 07:07:30 -0600

    moved listing VMs to separate folder

---
+++ MKTFY.Models/ViewModels/Listing/ListingVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKTFY.Models.ViewModels
+{
+    class ListingVM
+    {
+    }
+}
