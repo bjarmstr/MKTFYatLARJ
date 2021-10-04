86f4776f8e4657ad666c218ce8f7317a42ed7fb5
Jean Armstrong <bjarmstr@yahoo.com>
2021-10-04 07:55:46 -0600

    basic CRUD options complete

--- MKTFY.API/Controllers/ListingController.cs
+++ MKTFY.API/Controllers/ListingController.cs
@@ -71,5 +71,47 @@ namespace MKTFY.API.Controllers
             }
         }
 
+        [HttpPut]
+
+        public async Task<ActionResult<ListingVM>> Update([FromBody] ListingUpdateVM data)
+        {
+            try
+            {
+                var result = await _listingService.Update(data);
+                return Ok(result);
+
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { message = "Database Error" });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] Guid id)
+        {
+            try
+            {
+                // Tell the repository to delete the requested Listing entity
+                await _listingService.Delete(id);
+
+                // Return a 200 response
+                return Ok();
+            }
+            catch (DbUpdateException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Unable to contact the database" });
+            }
+            catch
+            {
+                return BadRequest(new { message = "Unable to delete the requested listing" });
+            }
+        }
     }
 }
