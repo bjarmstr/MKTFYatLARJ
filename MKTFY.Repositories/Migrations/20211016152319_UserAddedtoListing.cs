b0e409f05071256d425f669a58b483a0088cc132
Jean Armstrong <bjarmstr@yahoo.com>
2021-10-16 09:26:42 -0600

    added migrations

---
+++ MKTFY.Repositories/Migrations/20211016152319_UserAddedtoListing.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MKTFY.Repositories.Migrations
+{
+    public partial class UserAddedtoListing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Listings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Listings_UserId",
+                table: "Listings",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Listings_Users_UserId",
+                table: "Listings",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Listings_Users_UserId",
+                table: "Listings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Listings_UserId",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Listings");
+        }
+    }
+}
