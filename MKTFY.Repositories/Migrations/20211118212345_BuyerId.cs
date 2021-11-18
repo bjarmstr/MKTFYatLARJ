2864f1546c85bb8edd139d54fffd66fb731ed844
Jean Armstrong <bjarmstr@yahoo.com>
2021-11-18 14:24:42 -0700

    migration for buyerId

---
+++ MKTFY.Repositories/Migrations/20211118212345_BuyerId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MKTFY.Repositories.Migrations
+{
+    public partial class BuyerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerId",
+                table: "Listings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerId",
+                table: "Listings");
+        }
+    }
+}
