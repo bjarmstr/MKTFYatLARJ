214f54eacb62c2b1984c0b6d0d7ab0d7de7e869c
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-04 20:46:21 -0700

    user status migration

---
+++ MKTFY.Repositories/Migrations/20211205034507_UserStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MKTFY.Repositories.Migrations
+{
+    public partial class UserStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Users");
+        }
+    }
+}
