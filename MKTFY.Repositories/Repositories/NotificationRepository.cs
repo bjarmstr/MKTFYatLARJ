47bfcacce5f414ceba2ee5e43e02854272765e8b
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-09 12:09:23 -0700

    basic notification repository

---
+++ MKTFY.Repositories/Repositories/NotificationRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MKTFY.Models.Entities;
+using MKTFY.Repositories.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKTFY.Repositories.Repositories
+{
+    public class NotificationRepository: INotificationRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Notification> Create(Notification src)
+        {
+            _context.Add(src);
+            await _context.SaveChangesAsync();
+            return src;
+        }
+
+        public async Task<List<Notification>> Get(string userId)
+        {
+            var results = await _context.Notifications
+               .Where(n => n.UserId == userId)
+               .Include(n => n.User)
+               .Include(n => n.Message)
+               .ToListAsync();
+            return results;
+        }
+
+    }
+}
